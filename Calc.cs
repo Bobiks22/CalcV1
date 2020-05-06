0b7b0bae4c9d815fccbf229b3ae4d1ac93a954da
Bobiks <Bobiks@MATISS-GETAC>
2020-05-06 17:48:20 +0300

    UI

---
+++ Calc.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CalcV1
+{
+    public partial class Calc : Form
+    {
+        public Calc()
+        {
+            InitializeComponent();
+            InitCalc();
+        }
+
+        public void InitCalc()
+        {
+            string buttonName = null;
+            Button button = null; 
+            for (int i = 0; i <= 9; i++)
+            {
+                buttonName = "button" + i;
+                button = (Button)this.Controls[buttonName];
+                button.Text = i.ToString();
+            }
+        }
+    }
+}
